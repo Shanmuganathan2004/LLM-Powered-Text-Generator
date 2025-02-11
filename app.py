4267b5f68142591c882c8156d650f68bfee74155
shanmuganathan120 <137673464+Shanmuganathan2004@users.noreply.github.com>
2025-02-11 21:37:57 +0530

    Update app.py
    Update1. 2

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI #import fastapi
 import requests #sending requests to ollama
 
 app = FastAPI() 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+OLLAMA_URL = "http://localhost:11434/api/generate" #api key to use the llm
 
-@app.post("/generate/")
+@app.post("/generate/") #sending post requests
 def generate_text(prompt: str, word_limit: int = 100):
     payload = {"model": "deepseek-r1", "prompt": f"Generate {word_limit} words:\n\n{prompt}", "stream": False}
     response = requests.post(OLLAMA_URL, json=payload)
     return response.json().get("response", "No content generated.")
 
-# Run with: uvicorn app:app --reload
+# Run with: uvicorn app:app --reload # this cmd helps to run the app.py
